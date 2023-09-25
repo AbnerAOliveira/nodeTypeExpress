54f30ccb71bd5c773f8f1f5e54fa923c3bf65aea
Abner A Oliveira <abneralves10@hotmail.com>
2023-09-25 10:36:47 -0300

    feat: added create user method at repository

--- src/modules/users/repositories/mongoUser.repository.ts
+++ src/modules/users/repositories/mongoUser.repository.ts
@@ -1,9 +1,26 @@
 import { Mongo } from "../../../../database/mongo";
-import { IUserRepository, User } from "../models/user.model";
+import {
+  IUserRepository,
+  IUser,
+  ICreateUserParams,
+} from "../models/user.model";
 
 export class MongoUserRepository implements IUserRepository {
-  async getUsers(): Promise<User[]> {
-    const users = await Mongo.db.collection<User>("users").find({}).toArray();
+  async getUsers(): Promise<IUser[]> {
+    const users = await Mongo.db.collection<IUser>("users").find({}).toArray();
     return users;
+  }
+  async createUser(params: ICreateUserParams): Promise<IUser> {
+    const { insertedId } = await Mongo.db.collection("users").insertOne(params);
+
+    const user = await Mongo.db
+      .collection<IUser>("users")
+      .findOne({ _id: insertedId });
+
+    if (!user) {
+      throw "User not created";
+    }
+
+    return user;
   }
 }
